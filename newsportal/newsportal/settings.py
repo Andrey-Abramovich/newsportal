2bf90100b75993626040aa72eb3ec68b8d87a1dc
Andrey-Abramovich <aan_84@tut.by>
2022-03-03 02:20:25 +0300

    project setup

--- newsportal/newsportal/settings.py
+++ newsportal/newsportal/settings.py
@@ -157,5 +157,6 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+
 ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
 
--- newsportal/sign/urls.py
+++ newsportal/sign/urls.py
@@ -5,7 +5,7 @@ from .views import BaseRegisterView, upgrade_me
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'templates/sign/login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(template_name = 'templates/sign/logout.html'), name = 'logout'),
-    path('signup/', BaseRegisterView.as_view(template_name = 'templates/sign/signup.html'), name = 'signup'),
+    # path('signup/', BaseRegisterView.as_view(template_name = 'templates/sign/signup.html'), name = 'signup'),
     path('upgrade/', upgrade_me, name = 'upgrade'),
 
 ]
