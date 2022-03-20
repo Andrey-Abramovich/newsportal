32c8f1bf73f1c309b0243db4225829b803d10e92
andrey abramovich <aan_84@tut.by>
2022-03-20 12:18:50 +0300

    add M2M field "subscribers" at Category

--- newsportal/news/models.py
+++ newsportal/news/models.py
@@ -26,6 +26,7 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
+    subscribers = models.ManyToManyField(User, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
